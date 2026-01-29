3e942ee380b57f5b19eae9ca906976bdcb7fa78e
ekane Mac <ekanemacbelns@gmail.com>
2026-01-28 21:05:30 -0600

    changed admins name for new project

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "chirag-goyal-prsnl-project"
+    bucket         = "ekane demo-goyal-prsnl-project"
     key            = "global/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "tfstate-locks"
